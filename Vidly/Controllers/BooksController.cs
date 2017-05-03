ee4fe320586a4f85de7a04acde7f8556c3abff82
Ash Krishan <krishan.ash@hotmail.com>
2017-05-03 13:24:50 +0100

    First commit

---
+++ Vidly/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books/Random
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = @"C# Principles Advanced" };
+            return View(book);
+        }
+    }
+}
