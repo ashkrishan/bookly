dc375f902e500d92f76886b974e6cfed81e8281d
Ash Krishan <krishan.ash@hotmail.com>
2017-05-05 16:17:52 +0100

    First code commit

--- Vidly/Controllers/BooksController.cs
+++ Vidly/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,8 +13,51 @@ namespace Vidly.Controllers
         // GET: Books/Random
         public ActionResult Random()
         {
-            var book = new Book() { Name = @"C# Principles Advanced" };
-            return View(book);
+            var book = new Book
+            { Name = @"C# Principles Advanced" } ;
+            // return View(book);
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Jack Black"},
+                new Customer {Name = "Mr Tumble"}
+            };
+
+            var viewModel = new RandomViewModel
+            {
+                Book = book,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id: " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("?pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+            
+              
+        }
+       
+        
+        [Route("Books/{year:regex(\\d{4}):range(2004,2030)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("{0}/{1}", year, month));
+        }
+
     }
 }
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        
+        private readonly List<Customer> _customers = new List<Customer>
+
+            {
+                new Customer {Id=1, Name = "Mr Jack Brown"},
+                new Customer {Id=2, Name = "Miss Jane Smith"},
+                new Customer {Id=3,Name = "Lord Zack Smith"}
+            };
+
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = _customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/{details}/{id}")]
+        public ActionResult Details(int id)
+        {
+            
+            var getCustomer = (from cust in _customers
+                               where cust.Id == id
+                               select cust).FirstOrDefault();
+
+            return View(getCustomer);
+
+        }
+    }
+}
